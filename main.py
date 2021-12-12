30201e296ffb2e0598a36267609f06d1e762e0ff
AaravPatelRubixRobber <45376416+AaravPatelRubixRobber@users.noreply.github.com>
2021-12-12 17:12:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-#main file which compiles scores for all stocks
-
 import yfinance as yf
 import GrowthEstimateScraper
 import SentimentAnalysis
 import pandas as pd
+import os
 
 basicMaterials = ['APD', 'ACH', 'ASH', '500027', 'BHP', 'BRKM5', 'CHNR', 'CLW', 'CSNA3', 'EMN', 'FMC', 'FCX', 'FFHL', 'GGBR4', 'GLEN', '500440', 'IOSP', 'JMAT', 'KALU', 'KL', 'KLBN11', 'MMAT', 'NP', 'NTIC', 'PKX', 'KWR', 'ROLL', 'RIO', 'RGLD', 'SUZB3', '500470', 'X', 'VALE3', '500295']
 consumerDiscretionary = ['ANF', 'ATVI', 'AC', 'BABA', 'ATD.B', 'AMZN', 'AMC', 'AAL', 'AEO', 'APTV', 'AZO', 'AVP', '532977', 'BNED', '500043', 'BBBY', 'BBY', 'BWA', '500530', 'BYD', 'BBW', 'BURL', 'CZR', 'GOOS', 'KMX', 'CCL', 'CAKE', 'CAAS', 'CEA', 'ZNH', '601888', 'CMG', 'CPG', 'COST', 'DJCO', 'DAL', 'DG', 'DLTR', 'DOL', 'DOM', 'DPZ', 'EZJ', 'EDR', 'ETSY', 'FL', 'F', 'GPS', 'GM', 'HRB', 'HOG', 'HAS', '500182', 'HIBB', 'HD', 'HMC', 'HSW', 'H', 'IMAX', 'IHG', 'JBLU', '533155', 'LE', 'LVS', 'LCII', 'RENT3', 'LAME4', 'LREN3', 'LOOK', 'LULU', 'LYFT', 'M', 'MGLU3', '500520', 'MAR', '532500', 'MAT', 'MCD', '000333', 'MOV', 'NTES', 'NFLX', 'EDU', 'NXT', 'NKE', 'NCLH', 'OTB', 'ORLY', 'PZZA', 'PSON', 'PTON', 'PLNT', 'QSR', 'REV', 'RBA', 'SIRI', 'SKYW', 'SWBI', 'SONY', 'LUV', 'SAVE', 'SPOT', 'SBUX', 'SFIX', 'TAL', 'TGT', '500570', 'TSLA', 'TXRH', 'TCS', 'EL', 'DIS', 'WEN', 'NCTY', '500114', 'TJX', 'TM', 'UBER', 'ULTA', 'UAA', 'URBN', 'VRA', 'VFC', 'WMT', 'WTB', 'WSM', 'WH', 'YUM', 'ZNGA']
@@ -17,17 +16,6 @@ technology = ['ADBE', 'GOOG', 'AAPL', 'BIDU', 'BNFT', 'CTSH', 'CSU', 'DSCV', 'DX
 telecommunications = ['ANET', '532493', 'T', 'BCE', '532454', 'BT.A', '00941', 'CMCSA', '601138', 'GOGO', 'VIVT3', 'UTSI', 'VEON', 'VZ', 'VOD']
 utilities = ['AEP', 'AWK', 'CU', 'ELET3', 'ELET6', '00384', '600900', 'SBSP3', 'CMIG4', 'CPFE3', 'D', 'ENGI11', 'EGIE3', 'EQTL3', 'FE', '517300', 'HNP', 'MSEX', 'NFG', '532555', 'PCG', '532898', 'SRE', 'SVT', '533206', 'SJW', 'SO', 'SSE', '500400', 'YORW', 'UGI', 'UU.']
 
-"""basicMaterials = []
-consumerDiscretionary = []
-consumerStaples = []
-energy = []
-financials = []
-healthCare = []
-industrials = []
-realEstate = []
-technology = ['GOOG', 'AAPL']
-telecommunications = []
-utilities = []"""
 
 sectorIndices = {0: "basicMaterials",
              1: "consumerDiscretionary",
@@ -53,14 +41,13 @@ allStocks = [basicMaterials,
              telecommunications,
              utilities]
 
-#CALCULATES THE VALUE SCORE FOR A STOCK
 def calcScoreValue(ticker):
     print('Calculating value score...')
 
     score = 0
-    passCriteria = True
+    passCriteria = 1
 
-    #GETS TICKER
+    #GETS TICKER INFO
     try:
         stock = yf.Ticker(ticker)
         info = stock.info
@@ -70,27 +57,31 @@ def calcScoreValue(ticker):
         return -1
 
     #CALCULATES SCORES FOR EACH METRIC
+
+    #forwardPE
     try:
         forwardPE = 1000/info['forwardPE']
-        if info['forwardPE'] > 30 or info['forwardPE'] < 0: passCriteria = False
+        if info['forwardPE'] > 30 or info['forwardPE'] < 0: passCriteria -= 2
         print('forwardPE', info['forwardPE'])
     except:
         print('error with forward p/e... will try supplementing with trailing p/e')
         try:
             forwardPE = 1000/info['trailingPE']
-            if info['trailingPE'] > 30 or info['trailingPE'] < 0: passCriteria = False
+            if info['trailingPE'] > 30 or info['trailingPE'] < 0: passCriteria -= 2
             print('trailingPE', info['trailingPE'])
         except:
             print('error with trailing p/e')
             forwardPE = 0
 
+    #priceToBook
     try:
-        priceToBook = 25/info['priceToBook']
+        priceToBook = min(25/info['priceToBook'], 100)
         print('p/b', info['priceToBook'])
     except:
         print('error with priceToBook')
         priceToBook = 0
 
+    #dividend yeild
     try:
         if info["trailingAnnualDividendYield"] == None or info["trailingAnnualDividendYield"] == 0.0:
             dividendYeild = 0
@@ -102,28 +93,57 @@ def calcScoreValue(ticker):
         print('error with dividend yeild')
         dividendYeild = 0
 
+    #currentRatio
     try:
         currentRatio = 7*info['currentRatio']
-        if info['currentRatio'] < 0.7: passCriteria = False
-        elif info['currentRatio'] < 1: currentRatio = -75
+        if info['currentRatio'] < 0.7:
+            passCriteria -= 1
+            currentRatio = -75
+        elif info['currentRatio'] < 1: currentRatio = -35
         print('current ratio', info['currentRatio'])
     except:
         print('error with current ratio')
         currentRatio = 0
 
-    if not passCriteria:
+    #gross margins
+    try:
+        grossMargin = 15*info['grossMargins']**0.5
+        if info['grossMargins'] < 0.2: passCriteria -= 1
+        print('gross margin', info['grossMargins'])
+    except:
+        print('error with gross margin')
+        grossMargin = 0
+
+    #debt to Equity
+    try:
+        debtToEquity = (200-info['debtToEquity'])/8
+        if info['debtToEquity'] > 200 or info['debtToEquity'] < 0: passCriteria -= 1
+        print('debt to equity', info['debtToEquity'])
+    except:
+        print('error with debt to equity')
+        debtToEquity = 0
+
+    #insider ownership
+    try:
+        insiderOwnership = 25*info['heldPercentInsiders']**0.5
+        print('insider ownership', info['heldPercentInsiders'])
+    except:
+        print('error with insider ownership')
+        insiderOwnership = 0
+
+    if passCriteria < 0:
         print('failed criteria')
         return -1
 
+
     #CALCULATES FINAL SCORE
-    score = forwardPE + priceToBook + dividendYeild + currentRatio
+    score = forwardPE + priceToBook + dividendYeild + currentRatio + grossMargin + debtToEquity + insiderOwnership
     return score
 
-#CALCULATES THE GROWTH SCORE FOR A STOCK
 def calcScoreGrowth(ticker):
     print('Calculating growth score...')
     score = 0
-    passCriteria = True
+    passCriteria = 1
     # GETS TICKER
     try:
         stock = yf.Ticker(ticker)
@@ -134,41 +154,83 @@ def calcScoreGrowth(ticker):
         return -1
 
     # CALCULATES SCORES FOR EACH METRIC
+
+    #PEG ratio
     try:
         pegRatio = 100 / info['pegRatio']
-        if info['pegRatio'] > 4: passCriteria = False
+        if info['pegRatio'] > 4: passCriteria -= 2
         print('pegRatio', info['pegRatio'])
     except:
         print('error with pegRatio')
         pegRatio = 0
+
+    #estimated growth
     try:
         CalcGrowth = float(GrowthEstimateScraper.getLTGrowthRate(ticker))
-        if CalcGrowth < 15: passCriteria = False
+        if CalcGrowth < 15: passCriteria -= 2
         forcastedGrowth = 40*min(1+CalcGrowth/100, 1.7)**5
         print('forcastedGrowth', CalcGrowth)
+        #print('forwardEPS', info["forwardEPS"])
+        #print('trailingEPS', info["trailingEPS"])
     except:
         print('error with forcastedGrowth')
         forcastedGrowth = 0
+
+    #return on Equity
     try:
-        returnOnEquity = 100*min(info['returnOnEquity'], 1)
+        returnOnEquity = 200*min(info['returnOnEquity'], 0.7)
+        if info['returnOnEquity'] < 0.1:
+            passCriteria -= 1
+            returnOnEquity = -20
         print('returnOnEquity', info['returnOnEquity'])
     except:
         print('error with returnOnEquity')
         returnOnEquity = 0
+
+    #currentRatio
     try:
         currentRatio = 5*info['currentRatio']
-        if info['currentRatio'] < 0.7: passCriteria = False
-        elif info['currentRatio'] < 1: currentRatio = -75
+        if info['currentRatio'] < 0.7:
+            passCriteria -= 1
+            currentRatio = -75
+        elif info['currentRatio'] < 1: currentRatio = -35
         print('current ratio', info['currentRatio'])
     except:
         print('error with current ratio')
         currentRatio = 0
 
-    if not passCriteria:
+    #gross margins
+    try:
+        grossMargin = 15*info['grossMargins']**0.5
+        if info['grossMargins'] < 0.2: passCriteria -= 1
+        print('gross margin', info['grossMargins'])
+    except:
+        print('error with gross margin')
+        grossMargin = 0
+
+    #debt to Equity
+    try:
+        debtToEquity = (200-info['debtToEquity'])/8
+        if info['debtToEquity'] > 200 or info['debtToEquity'] < 0: passCriteria -= 1
+        print('debt to equity', info['debtToEquity'])
+    except:
+        print('error with debt to equity')
+        debtToEquity = 0
+
+    #insider ownership
+    try:
+        insiderOwnership = 25*info['heldPercentInsiders']**0.5
+        print('insider ownership', info['heldPercentInsiders'])
+    except:
+        print('error with insider ownership')
+        insiderOwnership = 0
+
+    if passCriteria < 0:
         print('failed criteria')
         return -1
 
-    score = pegRatio + forcastedGrowth + returnOnEquity + currentRatio
+
+    score = pegRatio + forcastedGrowth + returnOnEquity + currentRatio + grossMargin + debtToEquity + insiderOwnership
     return score
 
 def calcTechnicalScore(ticker):
@@ -181,8 +243,7 @@ def calcTechnicalScore(ticker):
     except:
         print('error with retrieving ticker')
         return -1
-    
-    #CALCULATES SCORE FOR EACH METRIC
+
     try:
         twoHundredDayAverage = info["twoHundredDayAverage"]
         fiftyDayAverage = info["fiftyDayAverage"]
@@ -206,61 +267,83 @@ def calcTechnicalScore(ticker):
     score = momentum + sentimentScore
     return score
 
-stockScores = {}
 
-sectorIndex = 0
-for sector in allStocks:
-    stockScores[sectorIndices[sectorIndex]] = {}
-    print('\n\n\n' + sectorIndices[sectorIndex].upper())
-    for stock in sector:
-        print(2 * '\n', '***' + stock + '***', '\n----------')
-        stockScoreValue = calcScoreValue(stock)
-        print()
-        stockScoreGrowth = calcScoreGrowth(stock)
-        print()
-        stockScoreTechnical = calcTechnicalScore(stock)
-        print(stock + ' scores: ', 'value-' + str(stockScoreValue) + ', growth-' + str(stockScoreGrowth) + ', technical-' + str(stockScoreTechnical))
-        stockScores[sectorIndices[sectorIndex]][stock] = [stockScoreValue, stockScoreGrowth, stockScoreTechnical]
-    sectorIndex += 1
+if __name__ == "__main__":
 
-print('stockScores', stockScores)
+    PATH = "YOURFILEPATH"#path csv file of stock scores should be exported to
+    stockScores = {}
 
-sectorIndex = 0
-for sector in stockScores.values():
+    #calculates all stock scores
+    sectorIndex = 0
+    for sector in allStocks:
+        stockScores[sectorIndices[sectorIndex]] = {}
+        print('\n\n\n' + sectorIndices[sectorIndex].upper())
+        for stock in sector:
+            try:
+                print(2 * '\n', '***' + stock + '***', '\n----------')
+                stockScoreValue = calcScoreValue(stock)
+                print()
+                stockScoreGrowth = calcScoreGrowth(stock)
+                print()
+                stockScoreTechnical = calcTechnicalScore(stock)
+                print(stock + ' scores: ', 'value-' + str(stockScoreValue) + ', growth-' + str(stockScoreGrowth) + ', technical-' + str(stockScoreTechnical))
+                stockScores[sectorIndices[sectorIndex]][stock] = [stockScoreValue, stockScoreGrowth, stockScoreTechnical]
+            except:
+                print('there was an ERROR')
+                stockScores[sectorIndices[sectorIndex]][stock] = [-1, -1, -1]
+        sectorIndex += 1
 
-    print('\n' + sectorIndices[sectorIndex])
-    sectorIndex += 1
+    print('stockScores', stockScores)
 
-    topValue = {}
-    topGrowth = {}
-    valueMinScore = 0
-    valueItems = max(int(len(sector)/5), 7)
-    growthMinScore = 0
-    growthItems = max(int(len(sector)/5), 7)
+    #filters out the top 20% of stocks per sector
+    sectorIndex = 0
+    for sector in stockScores.values():
 
-    for s, scores in sector.items():
-        if scores[0] > valueMinScore:
-            topValue[s] = scores[0]
-            if len(topValue) >= valueItems:
-                minKey = min(topValue, key=topValue.get)
-                del topValue[minKey]
+        sectorName = sectorIndices[sectorIndex]
+        print('\n' + sectorName)
+        sectorIndex += 1
 
-        if scores[1] > growthMinScore:
-            topGrowth[s] = scores[1]
-            if len(topGrowth) >= growthItems:
-                minKey = min(topGrowth, key=topGrowth.get)
-                del topGrowth[minKey]
+        topValue = {}
+        topGrowth = {}
+        valueMinScore = 0
+        valueItems = max(int(len(sector)/5), 7)
+        growthMinScore = 0
+        growthItems = max(int(len(sector)/5), 7)
 
-    topValueList = topValue.keys()
-    dfValue = pd.DataFrame({'stock': topValueList,
-               'value': [sector[stock][0] for stock in topValueList],
-               'technical': [sector[stock][2] for stock in topValueList]})
-    topGrowthList = topGrowth.keys()
-    dfGrowth = pd.DataFrame({'stock': topGrowthList,
-                            'growth': [sector[stock][1] for stock in topGrowthList],
-                            'technical': [sector[stock][2] for stock in topGrowthList]})
+        for s, scores in sector.items():
+            if scores[0] > valueMinScore:
+                topValue[s] = scores[0]
+                if len(topValue) >= valueItems:
+                    minKey = min(topValue, key=topValue.get)
+                    del topValue[minKey]
 
-    print("top Value:")
-    print(dfValue)
-    print("top Growth:")
-    print(dfGrowth)
+            if scores[1] > growthMinScore:
+                topGrowth[s] = scores[1]
+                if len(topGrowth) >= growthItems:
+                    minKey = min(topGrowth, key=topGrowth.get)
+                    del topGrowth[minKey]
+
+        #converts to dataframes
+        topValueList = topValue.keys()
+        dfValue = pd.DataFrame({'stock': topValueList,
+                   'value': [sector[stock][0] for stock in topValueList],
+                   'technical': [sector[stock][2] for stock in topValueList]})
+        topGrowthList = topGrowth.keys()
+        dfGrowth = pd.DataFrame({'stock': topGrowthList,
+                                'growth': [sector[stock][1] for stock in topGrowthList],
+                                'technical': [sector[stock][2] for stock in topGrowthList]})
+
+        print("top Value:")
+        print(dfValue)
+        print("top Growth:")
+        print(dfGrowth)
+
+        #adds as a file
+        tempPATH = PATH + sectorName
+        try:
+            os.makedirs(tempPATH)
+        except:
+            pass
+
+        dfValue.to_csv(tempPATH + "\\KWHSStockScoresValue.csv")
+        dfGrowth.to_csv(tempPATH + "\\KWHSStockScoresGrowth.csv")
